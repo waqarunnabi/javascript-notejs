9c73f5d987af70b87bc36e0c7a1c26ac0099338d
Waqar Un Nabi <37914819+waqarunnabi@users.noreply.github.com>
2022-09-18 23:08:03 +0500

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+// Language: javascript
+
+window.alert('Hello, world!');
+alert("Hello Waqar");
+
+console.log(34);
+
+// document.write("write");
+
+// Variables
+
+var name = "Waqar";
+
+alert(name);
+console.log(name);
+
+// Data Types
+console.log(typeof name);
---
+++ index3.js
@@ -0,0 +1,11 @@
+var name = prompt("What is your name?");
+alert("Hello " + name);
+
+var salary = prompt("What is your basic salary?");
+salary = parseInt(salary * 3);
+alert("Your salary is " + salary);
+
+// parseInt() - converts string to integer
+// parseFloat() - converts string to float
+// Number() - converts string to number
+// String() - converts number to string
